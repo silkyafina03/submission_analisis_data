d1e9de7485aa11e82c0c43b2fe429639bd6a975a
Silky Afina Saly <164318797+silkyafina03@users.noreply.github.com>
2025-03-06 12:56:48 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # Load data
 @st.cache_data
 def load_data():
-    data = pd.read_csv("Data/day.csv")  # Pastikan path sesuai
+    data = pd.read_csv('Data/day.csv')  # Pastikan path sesuai
     data["dteday"] = pd.to_datetime(data["dteday"])
     return data
 
